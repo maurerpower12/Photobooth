c3cd37eb7d4489d922d4c69a25578c4a2639c631
Joe Maurer <josephmau12@gmail.com>
2024-08-03 16:06:42 -0700

    Updating main

--- main.js
+++ main.js
@@ -1,39 +1,51 @@
-const { app, BrowserWindow } = require('electron');
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
-//import electronDl from 'electron-dl';
-//const electronDl = require('electron-dl');
-//electronDl();
+// TODO: Find out why this doesn't work
+// import electronDl from 'electron-dl';
+// electronDl();
 
-
-function createWindow() {
-  const win = new BrowserWindow({
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     //kiosk: true, // Powerfull... not sure if we want this or not
     fullscreen: true,
     title: "Photo Booth",
     titleBarStyle: 'hidden',
     disableAutoHideCursor: true,
     webPreferences: {
-      preload: path.join(__dirname, 'renderer.js')
+      preload: path.join(__dirname, 'preload.js')
     }
   });
-
-  win.maximize();
-  win.loadFile('index.html');
+  mainWindow.maximize();
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html');
+  
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
